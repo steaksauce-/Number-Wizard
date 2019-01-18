551d8b4160db4845691752e49896ad5b89f6a9ea
Ben <philipb.gregory@gmail.com>
2019-01-17 22:18:32 -0600

    Initial Commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = PlayerPrefs.GetInt("PlayerMax");
+        NextGuess();
+    }
+
+    // Update is called once per frame
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+                             
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Assets/PlayerSelector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelector : MonoBehaviour
+{
+
+    [SerializeField] Text playerWelcome;
+    string player;
+    string welcome;
+    int min = 1;
+    int max;
+
+    public void OnPressElijah()
+    {
+        PlayerPrefs.SetInt("PlayerMax", 1000);
+        player = "Elijah";
+        WelcomePlayer();
+    }
+
+    public void OnPressStefan()
+    {
+        PlayerPrefs.SetInt("PlayerMax", 100);
+        player = "Stefan";
+        WelcomePlayer();
+    }
+
+    public void OnPressAlex()
+    {
+        PlayerPrefs.SetInt("PlayerMax", 10);
+        player = "Alex";
+        WelcomePlayer();
+    }
+
+    void WelcomePlayer()
+    {
+        max = PlayerPrefs.GetInt("PlayerMax");
+        welcome = "Welcome, " + player + "! Please pick a number between " + min + " and " + max + ". Keep it a secret though! I am going to try to guess it. When you are ready, press Enter to start!";
+        playerWelcome.text = welcome;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        GameObject SceneLoader = GameObject.Find("SceneLoader");
+        SceneLoader.GetComponent<SceneLoader>();
+        SceneLoader.GetComponent<SceneLoader>().LoadNextScene();
+    }
+
+ }
